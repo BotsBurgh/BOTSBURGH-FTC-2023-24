523333232fbe23a2ba0baf795b7bd62445b04da9
BD103 <59022059+BD103@users.noreply.github.com>
2023-09-29 10:30:18 -0400

    feat: add teleop state api

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/TeleOpState.kt
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.api
+
+/**
+ * A state machine for teleop used to configure behavior.
+ *
+ * Its current purpose it to display whether the robot is automatically target-locking an april tag
+ * or if it should control normally.
+ */
+object TeleOpState : API() {
+    enum class State {
+        Default,
+        AprilTagLock,
+    }
+
+    /**
+     * The single source of truth for what state the robot is in.
+     *
+     * Be very careful about changing this value, as it affects many other things.
+     */
+    var state = State.Default
+}
