743a1f98b808cd051d36d8560a0b418f1d6be074
BD103 <59022059+BD103@users.noreply.github.com>
2023-10-22 16:19:39 -0400

    feat: linear api class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/linear/LinearAPI.kt
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.api.linear
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import org.firstinspires.ftc.teamcode.utils.Resettable
+
+abstract class LinearAPI {
+    private var uninitializedOpMode: LinearOpMode? by Resettable { null }
+
+    protected val linearOpMode: LinearOpMode
+        get() = uninitializedOpMode
+            ?: throw NullPointerException("<API> has not been initialized with the OpMode before being used.")
+
+    open fun init(linearOpMode: LinearOpMode) {
+        // You can only initialize an API once. If it is initialized more than once, throw an error.
+        if (uninitializedOpMode != null) {
+            throw IllegalStateException("Tried to initialize an <API> more than once.")
+        }
+
+        this.uninitializedOpMode = this.linearOpMode
+    }
+}
