b30d70886675ce411ef49fef121b4cf94138e600
cadence <117321183+cadenceforney@users.noreply.github.com>
2023-11-09 07:10:17 -0700

    BD103 changes
    CRServos for flywheels
    reset function
    stop Conveyor function
    reverse flywheel movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/InTake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/InTake.kt
@@ -1,20 +1,27 @@
 package org.firstinspires.ftc.teamcode.api
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.CRServo
 import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.hardware.DcMotor
 
 object InTake : API() {
-    lateinit var flyWheel1: Servo
-    lateinit var flyWheel2: Servo
+    lateinit var flyWheel1: CRServo
+    lateinit var flyWheel2: CRServo
 
     lateinit var inTakeConvey: DcMotor
+
+    //lateinit var flyWheelEncoder:
     override fun init(opMode: OpMode) {
         super.init(opMode)
 
-        this.flyWheel1 = hardwareMap.get(Servo::class.java, "flyWheel1")
-        this.flyWheel2 = hardwareMap.get(Servo::class.java, "flyWheel2")
+        this.flyWheel1 = hardwareMap.get(CRServo::class.java, "flyWheel1")
+        this.flyWheel2 = hardwareMap.get(CRServo::class.java, "flyWheel2")
         this.inTakeConvey = hardwareMap.get(DcMotor::class.java, "inTakeConvey")
+        resetEncoders()
+    }
+
+    fun resetEncoders() {
         inTakeConvey.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         inTakeConvey.mode = DcMotor.RunMode.RUN_USING_ENCODER
     }
@@ -23,12 +30,37 @@ object InTake : API() {
      */
     fun conveyMovement() {
         inTakeConvey.power = 1.0
-}
+    }
+    /*
+}   stop movement of Conveyor Belt
+    */
+    fun ConveyorStop() {
+        inTakeConvey.power = 0.0
+    }
     /*
     Moves the flyWheel
     */
     fun flyWheelMovement() {
-        flyWheel1.position += 5.0
-        flyWheel2.position += 5.0
+        flyWheel1.power = 1.0
+        flyWheel2.power = 1.0
+    }
+    /*
+    moves the flyWheel in reverse
+     */
+    fun flyWheelReverseMovement() {
+        flyWheel1.power = -1.0
+        flyWheel2.power = -1.0
+    }
+
+    // functions to be used during auto to move flyWheel and Conveyor Belt
+    fun flyWheelPosition(final: Int) {
+      //  while ( flyWheelEncoder.currentPosition < final) {
+        //    flyWheelMovement()
+       // }
+    }
+    fun conveyPosition(final: Int) {
+        while (inTakeConvey.currentPosition < final) {
+            conveyMovement()
+        }
     }
 }
