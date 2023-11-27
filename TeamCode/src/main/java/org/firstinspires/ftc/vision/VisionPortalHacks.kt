ff5105e98c34e6365fd7eb468c0409515f476de4
BD103 <59022059+BD103@users.noreply.github.com>
2023-11-27 11:16:20 -0500

    chore: add example for streaming camera to ftc dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/vision/VisionPortalHacks.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/vision/VisionPortalHacks.kt
@@ -7,6 +7,15 @@ import org.openftc.easyopencv.OpenCvCamera
  *
  * This is in the `org.firstinspires.ftc.vision` package so it can access the `protected`
  * [VisionPortalImpl.camera] property.
+ *
+ * # Example
+ *
+ * Stream the vision portal's camera to FTC Dashboard.
+ *
+ * ```
+ * val camera = getVisionPortalCamera(Vision.portal)!!
+ * FtcDashboard.getInstance().startCameraStream(camera, 0.0)
+ * ```
  */
 fun getVisionPortalCamera(visionPortal: VisionPortal): OpenCvCamera? =
     // Return the camera if visionPortal is a VisionPortalImpl, else null
