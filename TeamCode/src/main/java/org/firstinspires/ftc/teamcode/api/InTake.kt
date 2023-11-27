7603b1deb365e3ffbe52b5f96dbe249d86d9465a
BD103 <59022059+BD103@users.noreply.github.com>
2023-11-27 11:38:33 -0500

    chore: clean up InTake API

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/InTake.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/InTake.kt
@@ -2,22 +2,23 @@ package org.firstinspires.ftc.teamcode.api
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.hardware.CRServo
-import com.qualcomm.robotcore.hardware.Servo
 import com.qualcomm.robotcore.hardware.DcMotor
 
 object InTake : API() {
     lateinit var flyWheel1: CRServo
+        private set
     lateinit var flyWheel2: CRServo
-
+        private set
     lateinit var inTakeConvey: DcMotor
+        private set
 
-    //lateinit var flyWheelEncoder:
     override fun init(opMode: OpMode) {
         super.init(opMode)
 
         this.flyWheel1 = hardwareMap.get(CRServo::class.java, "flyWheel1")
         this.flyWheel2 = hardwareMap.get(CRServo::class.java, "flyWheel2")
         this.inTakeConvey = hardwareMap.get(DcMotor::class.java, "inTakeConvey")
+
         resetEncoders()
     }
 
@@ -25,27 +26,31 @@ object InTake : API() {
         inTakeConvey.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         inTakeConvey.mode = DcMotor.RunMode.RUN_USING_ENCODER
     }
-    /*
-    movement of Conveyor Belt
+
+    /**
+     * Starts moving the conveyor belt.
      */
     fun conveyMovement() {
         inTakeConvey.power = 1.0
     }
-    /*
-}   stop movement of Conveyor Belt
-    */
+
+    /**
+     * Stops the conveyor belt.
+     */
     fun conveyorStop() {
         inTakeConvey.power = 0.0
     }
-    /*
-    Moves the flyWheel
-    */
+
+    /**
+     * Starts moving the flywheels.
+     */
     fun flyWheelMovement() {
         flyWheel1.power = 1.0
         flyWheel2.power = 1.0
     }
-    /*
-    moves the flyWheel in reverse
+
+    /**
+     * Starts moving the flywheels in reverse.
      */
     fun flyWheelReverseMovement() {
         flyWheel1.power = -1.0
@@ -56,9 +61,10 @@ object InTake : API() {
     fun flyWheelPosition(final: Int) {
         // commented out as no current way to check for position
         //while ( flyWheelEncoder.currentPosition < final) {
-            //flyWheelMovement()
-       // }
+        //flyWheelMovement()
+        // }
     }
+
     fun conveyPosition(final: Int) {
         while (inTakeConvey.currentPosition < final) {
             conveyMovement()
