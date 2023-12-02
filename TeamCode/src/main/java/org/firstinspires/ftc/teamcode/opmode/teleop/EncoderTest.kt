b478b156c800fb4d00452edd5cef071151ec0fec
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-02 09:12:52 -0500

    feat: encoder test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/EncoderTest.kt
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.opmode.teleop
+
+import com.acmerobotics.dashboard.config.Config
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.api.TriWheels
+import org.firstinspires.ftc.teamcode.api.linear.Encoders
+import org.firstinspires.ftc.teamcode.utils.Reset
+
+@TeleOp(name = "Encoder Test")
+class EncoderTest : LinearOpMode() {
+    @Config
+    object Test {
+        @JvmField
+        var DISTANCE: Double = 24.0
+    }
+
+    override fun runOpMode() {
+        Reset.init(this)
+
+        TriWheels.init(this)
+        Encoders.init(this)
+
+        waitForStart()
+
+        Encoders.driveTo(Encoders.Direction.Red, Test.DISTANCE)
+    }
+}
