fdcb4b153113c796a908c8abbf23722024c37969
cadence <117321183+cadenceforney@users.noreply.github.com>
2023-12-03 17:50:30 -0500

    Box API
    initialized servos
    created functions to move box

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Box.kt
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.api
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.DcMotor
+import com.qualcomm.robotcore.hardware.Servo
+
+object Box : API() {
+
+    private lateinit var boxL: Servo
+    private lateinit var boxR: Servo
+    private lateinit var grip: Servo
+
+    override fun init(opMode : OpMode) {
+
+        super.init(opMode)
+
+        this.boxL = this.hardwareMap.get(Servo::class.java, "boxL")
+        this.boxR = this.hardwareMap.get(Servo::class.java, "boxR")
+        this.grip = this.hardwareMap.get(Servo::class.java, "grip")
+    }
+
+    /*
+    * Moves the box to the ground
+    */
+    fun dropBox() {
+        boxL.position = 90.0
+        boxR.position = 90.0
+    }
+
+    /*
+    * Moves box to upright position
+    */
+    fun pickUpBox() {
+        boxL.position = 0.0
+        boxL.position = 0.0
+    }
+
+    /*
+    * Moves the grip to keep pixels in place when  [pickUpBox] the box
+    */
+    fun gripIn() {
+        grip.position = 20.0
+    }
+
+    /*
+    * Resets the grip to starting position
+    */
+    fun gripOut() {
+        grip.position = 0.0
+    }
+}
