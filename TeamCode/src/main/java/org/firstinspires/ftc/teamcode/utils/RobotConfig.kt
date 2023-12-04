5598445c2e2e30a4e816e2d76949ae3afdf242fa
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-04 16:32:56 -0500

    feat: change default model config + ticks per inch
    TICKERS_PER_INCH now dynamically changes depending on what model of the robot is selected at build time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotConfig.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/RobotConfig.kt
@@ -20,7 +20,7 @@ object RobotConfig {
      *
      * @see Model
      */
-    val model: Model = Model.RobotB
+    val model: Model = Model.RobotA
 
     /**
      * When true, enables debugging features like camera streaming and more logs.
@@ -48,7 +48,10 @@ object RobotConfig {
          * increase accuracy.
          */
         @JvmField
-        var TICKS_PER_INCH: Double = 27.0
+        var TICKS_PER_INCH: Double = when (model) {
+            Model.RobotA -> 27.0
+            Model.RobotB -> 44.0
+        }
 
         /**
          * How many ticks a wheel needs to rotate for the robot to spin a single degree.
