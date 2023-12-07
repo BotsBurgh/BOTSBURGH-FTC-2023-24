4e3d91ea6c36c2f9c0c0af51a097eb6cd73c64fb
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-07 11:18:02 -0500

    chore: format box api

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Box.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Box.kt
@@ -1,17 +1,14 @@
 package org.firstinspires.ftc.teamcode.api
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
-import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.Servo
 
 object Box : API() {
-
     private lateinit var boxL: Servo
     private lateinit var boxR: Servo
     private lateinit var grip: Servo
 
-    override fun init(opMode : OpMode) {
-
+    override fun init(opMode: OpMode) {
         super.init(opMode)
 
         this.boxL = this.hardwareMap.get(Servo::class.java, "boxL")
@@ -19,32 +16,32 @@ object Box : API() {
         this.grip = this.hardwareMap.get(Servo::class.java, "grip")
     }
 
-    /*
-    * Moves the box to the ground
-    */
+    /**
+     * Moves the box to the ground
+     */
     fun dropBox() {
         boxL.position = 0.5
         boxR.position = 0.5
     }
 
-    /*
-    * Moves box to upright position
-    */
+    /**
+     * Moves box to upright position
+     */
     fun pickUpBox() {
         boxL.position = 0.0
         boxL.position = 0.0
     }
 
-    /*
-    * Moves the grip to keep pixels in place when  [pickUpBox] the box
-    */
+    /**
+     * Moves the grip to keep pixels in place when [pickUpBox] the box
+     */
     fun gripIn() {
         grip.position = 0.2
     }
 
-    /*
-    * Resets the grip to starting position
-    */
+    /**
+     * Resets the grip to starting position
+     */
     fun gripOut() {
         grip.position = 0.0
     }
