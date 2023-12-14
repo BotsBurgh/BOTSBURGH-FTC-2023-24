6cef9cf7c7529593ec91b6478f0279101ffdf43c
cadence <117321183+cadenceforney@users.noreply.github.com>
2023-12-13 20:19:01 -0500

    comments
    added necessary comments to the code to add readability
    
    Co-Authored-By: Vifi5 <117041326+Vif15@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/LinearSlide.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/LinearSlide.kt
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.api
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.hardware.DcMotor
 
+/**
+ * An API for controlling the linearSlide.
+ */
 object LinearSlide : API() {
 
     private lateinit var hook: DcMotor
@@ -23,16 +26,23 @@ object LinearSlide : API() {
         this.hook = this.hardwareMap.get(DcMotor::class.java, "hook")
     }
 
+    /**
+     * The movement of the linearSlide
+     */
     fun positionSlide(power: Double) {
+        // checks if the power is positive
         if (power > 0.0 && hook.currentPosition < linearSlideConfig.max) {
+            // when close to the max of linear slide slow down using reduction slope
             if (hook.currentPosition > linearSlideConfig.max - 250) {
                 hook.power =
                     LINEAR_SLIDE_REDUCTION_SLOPE * (hook.currentPosition - linearSlideConfig.max)
             } else {
                 hook.power = power
             }
+        // checks if the power is negative
         } else if (power < 0.0 && hook.currentPosition > linearSlideConfig.min) {
             hook.power = power
+        // sets power to stop if the power is zero
         } else {
             hook.power = 0.0
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/TeleOpMovement.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/TeleOpMovement.kt
@@ -37,6 +37,7 @@ object TeleOpMovement : Component {
             joyMagnitude *= SLOW_MULTIPLIER
         }
 
+        // if right stick y is moved use the movement of the linear slide
         if (gamepad2.right_stick_y != 0f) {
             LinearSlide.positionSlide(gamepad2.right_stick_y.toDouble())
         }
