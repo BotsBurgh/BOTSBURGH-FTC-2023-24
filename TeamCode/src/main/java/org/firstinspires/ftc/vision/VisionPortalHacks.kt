2bb53fc4a203fadcfd8778b17ef983b45de3f869
BD103 <59022059+BD103@users.noreply.github.com>
2023-12-20 19:28:19 -0500

    chore: add developer note to getVisionPortalCamera

--- TeamCode/src/main/java/org/firstinspires/ftc/vision/VisionPortalHacks.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/vision/VisionPortalHacks.kt
@@ -16,6 +16,18 @@ import org.openftc.easyopencv.OpenCvCamera
  * val camera = getVisionPortalCamera(Vision.portal)!!
  * FtcDashboard.getInstance().startCameraStream(camera, 0.0)
  * ```
+ *
+ * # Developer Note
+ *
+ * This is only possible because the [VisionPortalImpl.camera] property is `protected`, not
+ * `private`. In Java, `protected` items are visible to classes in the same package. This function
+ * is in the [org.firstinspires.ftc.vision] package so that it can read and export that property.
+ *
+ * This is technically bad practice and could break between minor updates to FtcRobotController.
+ *
+ * This was originally written to support streaming the camera to FTC Dashboard without writing a
+ * custom vision processor that just copied the pixels over. For a real-life example, look at the
+ * `AutoMain` opmode.
  */
 fun getVisionPortalCamera(visionPortal: VisionPortal): OpenCvCamera? =
     // Return the camera if visionPortal is a VisionPortalImpl, else null
