e4e98424f147f0806a24859ec31943302a91c9e4
cadence <117321183+cadenceforney@users.noreply.github.com>
2024-01-14 15:34:56 -0500

    init the box api
    the box api needed to be initialized

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpMain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleop/TeleOpMain.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.opmode.teleop
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.api.Box
 import org.firstinspires.ftc.teamcode.api.GamepadEx
 import org.firstinspires.ftc.teamcode.api.Telemetry
 import org.firstinspires.ftc.teamcode.api.TriWheels
@@ -23,6 +24,9 @@ class TeleOpMain : OpMode() {
 
         // Advanced gamepad inputs
         GamepadEx.init(this)
+
+        // Box controls
+        Box.init(this)
 
         // Log that we are initialized
         Telemetry.sayInitialized()
