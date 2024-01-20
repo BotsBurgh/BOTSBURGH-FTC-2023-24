58c1f9403cc0e6538dbfa8eff4bd23c63b60b977
BD103 <59022059+BD103@users.noreply.github.com>
2024-01-19 21:11:52 -0500

    feat: linear slide locking

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/TeleOpMovement.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/components/TeleOpMovement.kt
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode.components
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.DcMotor
 import org.firstinspires.ftc.teamcode.api.LinearSlide
 import org.firstinspires.ftc.teamcode.api.TriWheels
+import org.firstinspires.ftc.teamcode.utils.Resettable
 import org.firstinspires.ftc.teamcode.utils.RobotConfig
 import kotlin.math.PI
 import kotlin.math.atan2
@@ -14,6 +16,8 @@ import kotlin.math.sqrt
  * Requires the [TriWheels] API.
  */
 object TeleOpMovement : Component {
+    private var slideLocked: Boolean by Resettable { false }
+
     override fun loop(opMode: OpMode) {
         // alias gamepad1
         val gamepad = opMode.gamepad1
@@ -33,12 +37,32 @@ object TeleOpMovement : Component {
             joyMagnitude *= RobotConfig.TeleOpMovement.SLOW_MULTIPLIER
         }
 
-        if (gamepad.dpad_down) {
-            LinearSlide.power(RobotConfig.TeleOpMovement.SLIDE_DOWN_POWER)
-        } else if (gamepad.dpad_up) {
-            LinearSlide.power(RobotConfig.TeleOpMovement.SLIDE_UP_POWER)
-        } else {
+        // Lock or unlock the slide "brake"
+        if (gamepad.dpad_left) {
+            slideLocked = true
+
+            with (LinearSlide.slide) {
+                targetPosition = currentPosition
+                mode = DcMotor.RunMode.RUN_TO_POSITION
+            }
+        } else if (gamepad.dpad_right) {
+            slideLocked = false
+
+            LinearSlide.slide.mode = DcMotor.RunMode.RUN_USING_ENCODER
             LinearSlide.stop()
+        }
+
+        if (slideLocked) {
+            LinearSlide.slide.power = 0.4
+        } else {
+            // Manually move slide up and down
+            if (gamepad.dpad_down) {
+                LinearSlide.power(RobotConfig.TeleOpMovement.SLIDE_DOWN_POWER)
+            } else if (gamepad.dpad_up) {
+                LinearSlide.power(RobotConfig.TeleOpMovement.SLIDE_UP_POWER)
+            } else {
+                LinearSlide.stop()
+            }
         }
 
         // movement of all wheels
