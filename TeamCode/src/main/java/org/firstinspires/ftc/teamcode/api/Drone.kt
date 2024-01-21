66099c5d99e2ca74e8b7035fd4c1b0c89339bec3
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-01-21 17:53:56 -0500

    Added the API Drone.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Drone.kt
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.api
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.Servo
+import org.firstinspires.ftc.teamcode.utils.RobotConfig
+
+/**
+ * An API to control the drone launcher
+ */
+object Drone: API() {
+    private lateinit var pin: Servo
+
+    override fun init(opMode: OpMode) {
+        super.init(opMode)
+
+        this.pin = this.hardwareMap.get(Servo::class.java, "pin")
+    }
+
+    /** Moves the pin to the open position*/
+    fun releasePin() {
+        this.pin.position = RobotConfig.Drone.OPEN_PIN
+    }
+
+
+}
