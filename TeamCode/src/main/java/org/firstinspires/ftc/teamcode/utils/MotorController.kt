0ec60caff9547d4254b07a2e26e26a0bd9aeff47
BD103 <59022059+BD103@users.noreply.github.com>
2024-02-21 22:22:25 -0500

    feat: ftc dashboard config motor controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MotorController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MotorController.kt
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.utils
 
 import com.qualcomm.robotcore.hardware.DcMotor
-import com.qualcomm.robotcore.hardware.PIDCoefficients
 import com.qualcomm.robotcore.util.ElapsedTime
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
@@ -25,12 +25,6 @@ class MotorController(
     private val target: Int,
     private val motor: DcMotor,
 ) {
-    companion object {
-        private val pid = PIDCoefficients(0.0, 0.0, 0.0)
-        private const val POWER_LIMIT = 0.5
-        private const val I_LIMIT = 1.0
-    }
-
     private val runtime = ElapsedTime()
 
     private var previousTime = 0.0
@@ -42,20 +36,20 @@ class MotorController(
         val currentTime = runtime.milliseconds()
         val currentError = target - motor.currentPosition
 
-        val p = pid.p * currentError
+        val p = RobotConfig.MotorController.pid.p * currentError
 
-        i += pid.i * currentError * (currentTime - previousTime)
+        i += RobotConfig.MotorController.pid.i * currentError * (currentTime - previousTime)
 
         // Clamp between [-MAX_I, MAX_I]
-        i = max(min(i, I_LIMIT), -I_LIMIT)
+        i = max(min(i, RobotConfig.MotorController.iLimit), -RobotConfig.MotorController.iLimit)
 
-        val d = pid.d * (currentError - previousError) / (currentTime - previousTime)
+        val d = RobotConfig.MotorController.pid.d * (currentError - previousError) / (currentTime - previousTime)
 
-        motor.power = max(min(p + i + d, POWER_LIMIT), -POWER_LIMIT)
+        motor.power = max(min(p + i + d, RobotConfig.MotorController.powerLimit), -RobotConfig.MotorController.powerLimit)
 
         previousTime = currentTime
         previousError = currentError
     }
 
-    fun isDone() = previousError == 0
+    fun isDone() = abs(previousError) < 5
 }
