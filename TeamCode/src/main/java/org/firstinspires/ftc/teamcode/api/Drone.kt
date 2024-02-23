11026afe3d86caa1d4d1f2825d159b275637943f
Vifi5 <117041326+Vif15@users.noreply.github.com>
2024-02-23 10:57:15 -0500

    Feat: Revert Drone Launcher to CRServo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Drone.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/api/Drone.kt
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.api
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
+import com.qualcomm.robotcore.hardware.CRServo
 import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorSimple
 
@@ -8,21 +9,21 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple
  * An API to control the drone launcher.
  */
 object Drone : API() {
-    private lateinit var droneLeft: DcMotor
-    private lateinit var droneRight: DcMotor
+    private lateinit var droneLeft: CRServo
+    private lateinit var droneRight: CRServo
 
     override fun init(opMode: OpMode) {
         super.init(opMode)
 
-        this.droneLeft = this.opMode.hardwareMap.get(DcMotor::class.java, "droneLeft")
-        this.droneRight = this.opMode.hardwareMap.get(DcMotor::class.java, "droneRight")
+        this.droneLeft = this.opMode.hardwareMap.get(CRServo::class.java, "droneLeft")
+        this.droneRight = this.opMode.hardwareMap.get(CRServo::class.java, "droneRight")
 
-        this.droneRight.direction = DcMotorSimple.Direction.REVERSE
+
     }
 
     fun spin() {
         this.droneLeft.power = 1.0
-        this.droneRight.power = 1.0
+        this.droneRight.power = -1.0
     }
 
     fun stop() {
